60786f76937a8d1e608d414cc96616a8344f1839
ChanBuviCoders <chandran122250@gmail.com>
2026-09-10 01:56:47 +0530

    #java file name changes 6

--- src/main/java/config/Entity/Payments.java
+++ src/main/java/config/Entity/Payments.java
@@ -8,12 +8,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "payments")
 public class Payments {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
